ccb2818b51afaaf65e478e063fe040843b797250
Andrew <andrewhcsmith@gmail.com>
2019-09-30 21:52:45 +1000

    'clean'

--- src/br_scrapper_functions.R
+++ src/br_scrapper_functions.R
@@ -9,18 +9,20 @@ teams = c("ATL","BRK", 'BOS','CHA','CHI', 'CHO','CLE','DAL','DEN','DET','GSW','H
          'LAL','MEM','MIA','MIL','MIN','NJN', 'NOH', 'NOP','NYK','OKC','ORL','PHI','PHO','POR','SAC','SAS','TOR','UTA','WAS')
 years = seq(2017,2018,1)
 
-teams_seasons = expand.grid(teams,years)
-teams_seasons$team_year = paste0(teams_seasons$Var1, '/', teams_seasons$Var2)
+teams_seasons = expand.grid(Team = teams, Season = years)
+teams_seasons$team_year = paste0(teams_seasons$Team, '/', teams_seasons$Season)
 
 
-url = 'https://www.oddsportal.com/basketball/usa/nba/results/webpage = read_html(url)'
-games_html = html_nodes(webpage,'table') #%>% html_nodes("img")
+# url = 'https://www.oddsportal.com/basketball/usa/nba/results/webpage = read_html(url)'
+# games_html = html_nodes(webpage,'table') #%>% html_nodes("img")
+# 
+# season_data = data.table(html_table(games_html, fill = T)[[1]])
+# season_data
 
-season_data = data.table(html_table(games_html, fill = T)[[1]])
-season_data
 
 season_scrapper = function(team_year){
   
+  team = gsub('/.*','',team_year)
   season_data = data.table()
   url = paste0('https://www.basketball-reference.com/teams/', team_year ,'/gamelog/')
   webpage = read_html(url)
@@ -32,6 +34,7 @@ season_scrapper = function(team_year){
     colnames(season_data)[4] = 'Home'
     season_data = season_data[,Rk:=as.numeric(Rk)]
     season_data = season_data[!is.na(Rk)]
+    season_data$Team = team
   } 
   
   return(season_data)
@@ -41,7 +44,7 @@ all_data = data.table()
 all_data = plapply(teams_seasons$team_year, season_scrapper)
 
 temp = do.call(rbind, all_data)
-fwrite(temp, 'C:/Users/asmi797/OneDrive/Documents/Leisure/NBA Analysis/All_Games2.csv')
+fwrite(temp, 'C:/Users/asmi797/OneDrive/Documents/Leisure/nba-game-predictor/data/br_games_data.csv')
 
 
 
---
+++ src/data_cleaning_functions.R
@@ -0,0 +1,11 @@
+# This script contains functions that cleans the output from the bsaketball reference scrapping
+
+raw_data = temp
+
+clean_br_data = function(raw_data){
+  
+  # Add a game Index
+  home
+  
+  
+}
